420cb2893e765cbd592cb376bf6977249926bb8c
Isak Pao <windperson@gmail.com>
2023-09-22 15:37:33 +0800

    Silo Server and Orleans Client implemented

---
+++ src/Hosting/Client/HelloWorld.Client.Console/Program.cs
@@ -0,0 +1,34 @@
+﻿using Orleans;
+using Orleans.Configuration;
+using static System.Console;
+
+using HelloWorld.Interfaces.Hello;
+
+WriteLine("\r\n---Orleans RPCDemo Client---");
+WriteLine("\r\n---\r\nInitializing Orleans Client...\r\n---");
+var client = new ClientBuilder()
+    .UseLocalhostClustering()
+    .Configure<ClusterOptions>(options =>
+    {
+        options.ClusterId = "console-client-01";
+        options.ServiceId = "Demo Greeting Service";
+    })
+    .ConfigureApplicationParts(parts =>
+    {
+        parts.AddApplicationPart(typeof(IHelloGrain).Assembly).WithReferences();
+    }).Build();
+
+WriteLine( "Please wait until Orleans Server is started and ready for connections, then press any key to start connect...");
+ReadKey();
+await client.Connect();
+WriteLine("\r\n---\r\nOrleans Client connected\r\n---");
+
+var helloGrain = client.GetGrain<IHelloGrain>(0);
+var helloResult = await helloGrain.SayHello("Orleans");
+WriteLine($"\r\n---\r\nCall HelloGrain.SayHello(\"Orleans\") =\r\n{helloResult}\r\n---");
+
+WriteLine("\r\n---\r\nDemonstration finished, press any key to exit...");
+ReadKey();
+
+await client.Close();
+client.Dispose();
---
+++ src/Hosting/Server/HelloWorld.Hosting.Console/Program.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Extensions.Logging;
+using Orleans;
+using Orleans.Configuration;
+using Orleans.Hosting;
+using static System.Console;
+
+using HelloWorld.Grains.Greeting;
+using System.Net;
+
+var siloHost = new SiloHostBuilder()
+    .UseLocalhostClustering()
+    .Configure<ClusterOptions>(options =>
+    {
+        options.ClusterId = "console-host-01";
+        options.ServiceId = "Demo Greeting Service";
+    })
+    .Configure<EndpointOptions>(options => options.AdvertisedIPAddress = IPAddress.Loopback)
+    .ConfigureApplicationParts(parts =>
+    {
+        parts.AddApplicationPart(typeof(HelloGrain).Assembly).WithReferences();
+    })
+    .ConfigureLogging(logging =>
+    {
+        logging.AddConsole();
+        logging.AddDebug();
+    })
+    .Build();
+
+//Tricks to manually wait for Ctrl+C key press
+var waitForProcessShutdown = new ManualResetEvent(false);
+CancelKeyPress += (sender, eventArgs) =>
+{
+    eventArgs.Cancel = true;
+    waitForProcessShutdown.Set();
+};
+
+await siloHost.StartAsync();
+WriteLine(
+    "===\r\nOrleans Silo started and able to connect,\r\nPress Ctrl+C to shutdown when client finish demonstration...\r\n===");
+waitForProcessShutdown.WaitOne();
+
+WriteLine("Shutting down Silo...");
+await siloHost.StopAsync().ConfigureAwait(false);
+WriteLine("===\r\nSilo shutdown complete, exiting...\r\n===");
+Environment.Exit(0);
+
